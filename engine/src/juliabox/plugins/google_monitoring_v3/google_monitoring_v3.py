6d3ad8ad5603dd240794df729d139c23364822f7
nkottary <kottarynishanth54@gmail.com>
2016-09-25 12:51:38 +0530

    Add zone argument

--- engine/src/juliabox/plugins/google_monitoring_v3/google_monitoring_v3.py
+++ engine/src/juliabox/plugins/google_monitoring_v3/google_monitoring_v3.py
@@ -1,4 +1,4 @@
-__author__ = "Nishanth"
+__author__ = 'Nishanth'
 
 import threading
 import datetime
@@ -17,10 +17,10 @@ class GoogleMonitoringV3(JBPluginCloud):
                 JBPluginCloud.JBP_MONITORING_GOOGLE_V3]
     threadlocal = threading.local()
 
-    RFC_3339_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-    ALLOWED_CUSTOM_GCE_VALUE_TYPES = ["DOUBLE", "INT64"]
-    ALLOWED_EC2_VALUE_TYPES = ["Percent", "Count"]
-    CUSTOM_METRIC_DOMAIN = "custom.googleapis.com/"
+    RFC_3339_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+    ALLOWED_CUSTOM_GCE_VALUE_TYPES = ['DOUBLE', 'INT64']
+    ALLOWED_EC2_VALUE_TYPES = ['Percent', 'Count']
+    CUSTOM_METRIC_DOMAIN = 'custom.googleapis.com/'
     SELF_STATS = dict()
 
     @staticmethod
@@ -28,7 +28,7 @@ class GoogleMonitoringV3(JBPluginCloud):
         c = getattr(GoogleMonitoringV3.threadlocal, 'cm_conn', None)
         if c is None:
             creds = GoogleCredentials.get_application_default()
-            GoogleMonitoringV3.threadlocal.cm_conn = c = build("monitoring", "v3",
+            GoogleMonitoringV3.threadlocal.cm_conn = c = build('monitoring', 'v3',
                                                                credentials=creds).projects()
         return c
 
@@ -39,38 +39,38 @@ class GoogleMonitoringV3(JBPluginCloud):
     @staticmethod
     def _process_value_type(value_type):
         if value_type in GoogleMonitoringV3.ALLOWED_EC2_VALUE_TYPES:
-            if value_type == "Count":
-                return "INT64"
-            return "DOUBLE"
+            if value_type == 'Count':
+                return 'INT64'
+            return 'DOUBLE'
         elif value_type in GoogleMonitoringV3.ALLOWED_CUSTOM_GCE_VALUE_TYPES:
             return value_type
         else:
-            raise Exception("Invalid value_type argument.")
+            raise Exception('Invalid value_type argument.')
 
     @staticmethod
     def _get_timeseries_dict(metric_name, labels, value, value_type, timenow, instance_id, zone):
         value_type = GoogleMonitoringV3._process_value_type(value_type)
-        if value_type == "DOUBLE":
+        if value_type == 'DOUBLE':
             value = value / 100
         timeseries = {
-            "metric": {
-                "type": GoogleMonitoringV3.CUSTOM_METRIC_DOMAIN + metric_name,
-                "labels": labels,
+            'metric': {
+                'type': GoogleMonitoringV3.CUSTOM_METRIC_DOMAIN + metric_name,
+                'labels': labels,
             },
-            "resource": {
-                "type": "gce_instance",
-                "labels": {
-                    "instance_id": instance_id,
-                    "zone": zone,
+            'resource': {
+                'type': 'gce_instance',
+                'labels': {
+                    'instance_id': instance_id,
+                    'zone': zone,
                 },
             },
-            "points": [
+            'points': [
                 {
-                    "interval": {
-                        "endTime": timenow,
+                    'interval': {
+                        'endTime': timenow,
                     },
-                    "value": {
-                        value_type.lower() + "Value": value
+                    'value': {
+                        value_type.lower() + 'Value': value
                     }
                 },
             ],
@@ -82,7 +82,7 @@ class GoogleMonitoringV3(JBPluginCloud):
     def _ts_write(timeseries, install_id):
         ts = GoogleMonitoringV3._connect_google_monitoring().timeSeries()
         return ts.create(name='projects/' + install_id,
-                         body={"timeSeries": timeseries}).execute()
+                         body={'timeSeries': timeseries}).execute()
 
     @staticmethod
     def _update_timeseries(timeseries):
@@ -112,7 +112,7 @@ class GoogleMonitoringV3(JBPluginCloud):
         for (stat_name, stat_unit, stat_value) in stats:
             if should_cache:
                 GoogleMonitoringV3.SELF_STATS[stat_name] = stat_value
-            GoogleMonitoringV3.log_info("CloudMonitoring %s.%s.%s=%r(%s)",
+            GoogleMonitoringV3.log_info('CloudMonitoring %s.%s.%s=%r(%s)',
                                         install_id, instance_id, stat_name,
                                         stat_value, stat_unit)
             timeseries.append(
@@ -149,11 +149,11 @@ class GoogleMonitoringV3(JBPluginCloud):
                         time.sleep(3)
                     else:
                         raise
-            series = resp.get("timeSeries")
+            series = resp.get('timeSeries')
             if series == None:
                 break
             retlist.extend(series[0]['points'])
-            nextpage = resp.get("nextPageToken")
+            nextpage = resp.get('nextPageToken')
             if nextpage == None:
                 break
         return retlist
@@ -162,7 +162,7 @@ class GoogleMonitoringV3(JBPluginCloud):
     def get_instance_stats(instance, stat_name, this_id, install_id,
                            autoscale_group):
         if (instance == this_id) and (stat_name in GoogleMonitoringV3.SELF_STATS):
-            GoogleMonitoringV3.log_debug("Using cached self_stats. %s=%r",
+            GoogleMonitoringV3.log_debug('Using cached self_stats. %s=%r',
                                          stat_name, GoogleMonitoringV3.SELF_STATS[stat_name])
             return GoogleMonitoringV3.SELF_STATS[stat_name]
         res = None
@@ -184,7 +184,7 @@ class GoogleMonitoringV3(JBPluginCloud):
         ts = GoogleMonitoringV3._connect_google_monitoring().timeSeries()
         nowtime = datetime.datetime.utcnow()
         filter = 'metric.type = "' + GoogleMonitoringV3.CUSTOM_METRIC_DOMAIN \
-                 + metric_name + '"\nmetric.label.GroupID = "' + autoscale_group + '"' 
+                 + metric_name + '"\nmetric.label.GroupID = "' + autoscale_group + '"'
         while True:
             start = time.time()
             metrics = None
@@ -201,16 +201,16 @@ class GoogleMonitoringV3(JBPluginCloud):
                         time.sleep(3)
                     else:
                         raise
-            series = metrics.get("timeSeries")
+            series = metrics.get('timeSeries')
             if series == None:
                 break
             for m in series:
-                for n_dim, v_dim in m['metric']["labels"].iteritems():
+                for n_dim, v_dim in m['metric']['labels'].iteritems():
                     dims[n_dim] = dims.get(n_dim, []) + [v_dim]
-            next_token = metrics.get("nextPageToken")
+            next_token = metrics.get('nextPageToken')
             if next_token is None:
                 break
         if len(dims) == 0:
-            GoogleMonitoringV3.log_warn("invalid metric " + '.'.join([install_id, metric_name]))
+            GoogleMonitoringV3.log_warn('invalid metric ' + '.'.join([install_id, metric_name]))
             return None
         return dims
